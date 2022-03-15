ede108515fd67d2fc050c182ff12fdee7b8d5f20
ltzan87 <100611591+ltzan87@users.noreply.github.com>
2022-03-15 17:27:39 -0300

    add cinemachine and jump animations

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
+    [Header("Seep Setup")]
     public Rigidbody2D myRigidbody;
     public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
     public float speedRun;
     public float forceJump = 2f;
+
+    [Header("Animation Setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = .7f;
+    public float animationDuration = .3f;
+    public Ease ease = Ease.OutBack;
 
     private float _currentSpeed;
 
@@ -48,6 +56,16 @@ public class Player : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
             {
                 myRigidbody.velocity = Vector2.up * forceJump;
+                myRigidbody.transform.localScale = Vector2.one;
+
+                DOTween.Kill(myRigidbody.transform);
+
+                HandleScaleJump();
             }
+    }
+
+    private void HandleScaleJump() {
+        myRigidbody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigidbody.transform.DOScaleY(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
 }
