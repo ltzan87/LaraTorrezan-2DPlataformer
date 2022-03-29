e3d65063b229d88f4efd96690efa5020881e80f1
ltzan87 <100611591+ltzan87@users.noreply.github.com>
2022-03-29 16:23:00 -0300

    add gun and bullet to player and damage to enemy

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -8,6 +8,7 @@ public class EnemyBase : MonoBehaviour
 
     public Animator animator;
     public string triggerAttack = "Attack";
+    public HealthBase healthBase;
 
     private void OnCollisionEnter2D(Collision2D collision) {
         Debug.Log(collision.transform.name);
@@ -24,5 +25,9 @@ public class EnemyBase : MonoBehaviour
 
     private void PlayAttackAnimation() {
         animator.SetTrigger(triggerAttack);
+    }
+
+    public void Damage(int amount) {
+        healthBase.Damage(amount);
     }
 }
